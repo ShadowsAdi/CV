f8a0d6406aad902f6f29d5d7be93a755854281aa
Andreea <76445060+MazereAndreea@users.noreply.github.com>
2024-02-14 13:34:49 +0200

    Learning how to map function works, as well as keys in a list

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -1,8 +1,16 @@
+
 function Greeting() {
+    
+    const alimente = ["mama", "tata", "alex"]
     return (
         <>
             <h1>&quot;looool&quot;</h1>
             <h1>Nush frt</h1>
+            {alimente.map(
+                (membru) => {
+                    return <li key = {membru}>{membru}</li>
+                }
+            )}
         </>
     );
 }
